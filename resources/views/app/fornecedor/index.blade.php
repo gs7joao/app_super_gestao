b5ccbe18784f13757d32c89eaeca135da20bf153
gs7joao <gsjoaopedro7@gmail.com>
2023-02-19 10:08:04 -0300

    Estudos sintaxe blade isset e empty

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -4,22 +4,59 @@
 
 {{-- Implementando sintaxe PHP para informar que é PHP Puro --}}
 @php
-//  if() {
-    
-//  } else if () {
-     
-//  } else {
-
-//  }
+    // if(!<$variavel>) {} // Executa se o retorno for true
+    // if(isset(<$variavel>)) {} // Retorna true se a variável estiver definida
+    // if(empty(<$variavel>)) {} // Retorna true se a variável estiver vazia
 @endphp
+
+
+{{-- @empty executa se a variável estiver vazia --}}
+{{-- @isset executa se a variável estiver definida --}}
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+        Status: {{ $fornecedores[0]['cnpj'] }}
+
+        {{-- @empty executa se a variável estiver vazia --}}
+        @empty($fornecedores[0]['cnpj'])
+            CNPJ VAZIO
+        @endempty
+    @endisset
+    <br>
+@endisset
+
+
+{{-- @unless executa se o retorno for false --}}
+{{-- Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
+@unless($fornecedores[0]['status'] == 'S') <!-- Executa se a condição for false -->
+Fornecedor Inativo
+@endunless
+<br>
+<br>
+Fornecedor: {{ $fornecedores[1]['nome'] }}
+<br>
+Status: {{ $fornecedores[1]['status'] }}
+<br>
+@unless($fornecedores[1]['status'] == 'N') <!-- Executa se a condição for false -->
+Fornecedor Ativo
+@endunless --}}
+
 
 {{-- Instrução para printar o array retornado --}}
 {{-- @dd($fornecedores) --}}
+
+
 {{-- Utilizando condição de IF/ElseIf e Else--}}
-@if(count($fornecedores) > 0 && count($fornecedores) <= 5)
+{{-- @if(count($fornecedores) > 0 && count($fornecedores) <= 5)
     <h3> Existem alguns fornecedores cadastrados </h3>
 @elseif(count($fornecedores) > 5)
     <h3> Existem vários fornecedores cadastrados </h3>
 @else
     <h3> Ainda não existem fornecedores cadastrados</h3>
-@endif
+@endif --}}
